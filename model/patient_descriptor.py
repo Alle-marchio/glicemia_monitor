fcf51c10cbd9d3e1d92e08d2cd9287736a55bce4
Alle-marchio <78793701+Alle-marchio@users.noreply.github.com>
2026-01-17 16:25:44 +0100

    refactor data_collector_consumer to remove unused comments and improve code clarity

--- model/patient_descriptor.py
+++ model/patient_descriptor.py
@@ -1,35 +1,31 @@
 import json
 import time
-from utils.senml_helper import SenMLHelper
 
 class PatientDescriptor:
-    """Descrittore del paziente diabetico con parametri di configurazione"""
-
     def __init__(self, patient_id, name, age, weight,
                  target_glucose_min, target_glucose_max,
                  hypoglycemia_threshold, hyperglycemia_threshold,
                  insulin_sensitivity_factor, carb_ratio, basal_insulin_rate,
                  sensor_reading_interval, alert_enabled, emergency_contact):
 
-        # Identificazione paziente
         self.patient_id = patient_id
         self.name = name
         self.age = age
-        self.weight = weight  # kg
+        self.weight = weight
 
-        # Parametri glicemici (mg/dL)
-        self.target_glucose_min = target_glucose_min  # Soglia minima normale
-        self.target_glucose_max = target_glucose_max  # Soglia massima normale
-        self.hypoglycemia_threshold = hypoglycemia_threshold  # Soglia ipoglicemia
-        self.hyperglycemia_threshold = hyperglycemia_threshold  # Soglia iperglicemia
+        # Parametri glicemici
+        self.target_glucose_min = target_glucose_min
+        self.target_glucose_max = target_glucose_max
+        self.hypoglycemia_threshold = hypoglycemia_threshold
+        self.hyperglycemia_threshold = hyperglycemia_threshold
 
         # Parametri insulina
-        self.insulin_sensitivity_factor = insulin_sensitivity_factor  # mg/dL per unità di insulina
-        self.carb_ratio = carb_ratio  # grammi di carboidrati per unità di insulina
-        self.basal_insulin_rate = basal_insulin_rate  # unità/ora
+        self.insulin_sensitivity_factor = insulin_sensitivity_factor
+        self.carb_ratio = carb_ratio
+        self.basal_insulin_rate = basal_insulin_rate
 
         # Configurazione sistema
-        self.sensor_reading_interval = sensor_reading_interval  # minuti
+        self.sensor_reading_interval = sensor_reading_interval
         self.alert_enabled = alert_enabled
         self.emergency_contact = emergency_contact
 
@@ -38,19 +34,15 @@ class PatientDescriptor:
         self.last_updated = int(time.time())
 
     def is_glucose_in_target_range(self, glucose_value):
-        """Verifica se il valore glicemico è nel range target"""
         return self.target_glucose_min <= glucose_value <= self.target_glucose_max
 
     def is_hypoglycemic(self, glucose_value):
-        """Verifica se il valore indica ipoglicemia"""
         return glucose_value < self.hypoglycemia_threshold
 
     def is_hyperglycemic(self, glucose_value):
-        """Verifica se il valore indica iperglicemia"""
         return glucose_value > self.hyperglycemia_threshold
 
     def calculate_insulin_dose(self, current_glucose, target_glucose=None):
-        """Calcola dose insulina necessaria per correzione"""
         if target_glucose is None:
             target_glucose = (self.target_glucose_min + self.target_glucose_max) / 2
 
@@ -61,7 +53,6 @@ class PatientDescriptor:
         return glucose_difference / self.insulin_sensitivity_factor
 
     def to_json(self):
-        """Converte il modello in JSON per invio MQTT"""
         return json.dumps(self, default=lambda o: o.__dict__)
 
     def to_senml(self):
